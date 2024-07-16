3c6d37c33ee3644762598d2048bd16ec27fefb50
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-16 16:17:15 +0000

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,12 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """ destroys a session """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        self._db.update_user(user.id, session_id=None)
+        return None
