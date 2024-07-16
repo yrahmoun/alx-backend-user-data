839feecbf448c8a97f34143488cf04aaf30884e1
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-07-16 16:28:42 +0000

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,5 +47,18 @@ def log_in() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """ user log out """
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
